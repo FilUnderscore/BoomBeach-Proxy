218c16374b8b30f80696002d96646c8f62b389be
filip <filjerkovic@gmail.com>
2017-10-23 00:08:35 +1300

    cryptography

--- src/proxy/crypto/sodium/servercrypto.cpp
+++ src/proxy/crypto/sodium/servercrypto.cpp
@@ -6,6 +6,7 @@
  */
 
 #include "../../../include/proxy/crypto/sodium/servercrypto.hpp"
+#include <cstring>
 
 servercrypto::servercrypto(string magicKey)
 {
@@ -23,10 +24,55 @@ servercrypto::servercrypto(string magicKey)
 
 void basecrypto::decryptPacket(message message)
 {
+	if(message.getHeader().getId() == messagemap::getId("ClientHello"))
+	{
+		if(magicKey.len != 0)
+		{
+			serverKey = bytes::fromHexString("");
+		}
 
+		message.setDecryptedPayload(message.getEncryptedPayload());
+	}
+	else if(message.getHeader().getId() == messagemap::getId("ClientLogin"))
+	{
+		clientKey = *new byte_array(32);
+
+		memcpy(clientKey.buffer, message.getEncryptedPayload().buffer, 32);
+
+		if(magicKey.len != 0)
+		{
+			sharedKey = magicKey;
+		}
+		else
+		{
+			sharedKey = *new byte_array(32);
+			//Undefined reference?
+			//crypto_box_beforenm(sharedKey.buffer, clientKey.buffer, privateKey.buffer);
+		}
+
+		nonce nc = *new nonce(clientKey, serverKey);
+
+
+	}
+	else
+	{
+
+	}
 }
 
 void basecrypto::encryptPacket(message message)
 {
+	if(message.getHeader().getId() == messagemap::getId("ServerHello")
+	|| message.getHeader().getId() == messagemap::getId("ServerLoginBad"))
+	{
+		message.setEncryptedPayload(message.getDecryptedPayload());
+	}
+	else if(message.getHeader().getId() == messagemap::getId("ServerLoginOk"))
+	{
 
+	}
+	else
+	{
+
+	}
 }
